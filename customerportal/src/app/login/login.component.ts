c39e421a6831392cda8f19264a3f1c05f1619279
shrilaxmi1800 <54523735+shrilaxmi1800@users.noreply.github.com>
2021-02-28 19:38:56 +0530

    commit

--- customerportal/src/app/login/login.component.ts
+++ customerportal/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,5 +11,5 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  
 }
