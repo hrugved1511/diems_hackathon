50a9130238318a4144339ba7b8f3d56be2303b74
medha <medhachikshe99@gmail.com>
2021-02-28 18:02:25 +0530

    commit

--- industryportal/src/app/app-routing.module.ts
+++ industryportal/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent},
-  {path:'',redirectTo:'/home',pathMatch:'full'}
+  {path:'',redirectTo:'/login',pathMatch:'full'}
 ];
 
 @NgModule({
